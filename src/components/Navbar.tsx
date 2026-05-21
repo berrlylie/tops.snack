ebdba3f5e309b693842a7c3d9067f03c212c4386
amanda devina <amandadevina83@gmail.com>
2026-05-21 22:03:03 +0700

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -38,7 +38,7 @@ export default function Navbar() {
               <img 
                 src="/Logotopssnack.png"
                 alt="Logo Tops Snack" 
-                className="w-15 h-15 object-contain"
+                className="w-20 h-20 object-contain"
               />
               <span className="text-2xl font-bold text-brand-brown-dark tracking-tight font-serif">
                 Tops <span className="text-brand-green-leaf">Snack</span>
