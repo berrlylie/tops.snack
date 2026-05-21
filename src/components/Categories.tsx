8ff75dc4735fd939ae588183f7193e429f62735e
amanda devina <amandadevina83@gmail.com>
2026-05-22 00:43:54 +0700

    Update Categories.tsx

--- src/components/Categories.tsx
+++ src/components/Categories.tsx
@@ -19,7 +19,6 @@ export default function Categories() {
     <section className="py-24 bg-brand-beige">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="text-center mb-16">
-          <h2 className="text-sm font-bold text-brand-green-leaf uppercase tracking-widest mb-4">Kategori</h2>
           <h3 className="text-4xl font-bold text-brand-brown-dark">Pilihan Menu Kami</h3>
         </div>
 
