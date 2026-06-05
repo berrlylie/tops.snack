11bdd8bf9cf4cc86834d79c85b4f55a18cca95db
amanda devina <amandadevina83@gmail.com>
2026-06-05 10:23:32 +0700

    Tambah API

---
+++ api/index.py
@@ -0,0 +1,30 @@
+from flask import Flask, jsonify, request
+import psycopg2
+import os
+
+app = Flask(__name__)
+
+def get_db_connection():
+    # Mengambil "kunci" database POSTGRES_URL dari Vercel
+    conn = psycopg2.connect(os.environ['POSTGRES_URL'])
+    return conn
+
+# Jalur untuk mengambil data (nanti dipakai pembeli dan admin)
+@app.route('/api', methods=['GET'])
+def get_produk():
+    try:
+        conn = get_db_connection()
+        cur = conn.cursor()
+        cur.execute('SELECT * FROM produk ORDER BY id ASC;')
+        produk = cur.fetchall()
+        cur.close()
+        conn.close()
+        
+        # Merapikan data agar mudah dibaca website
+        hasil = []
+        for p in produk:
+            hasil.append({"id": p[0], "nama": p[1], "harga": p[2], "gambar": p[3]})
+            
+        return jsonify(hasil)
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
